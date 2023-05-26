062bdee69840e68f800f59a5fe609b90276bee87
3sky <kuba@3sky.dev>
2023-05-26 15:09:00 +0200

    Finnaly working solution

--- test/cdk-ts.test.ts
+++ test/cdk-ts.test.ts
@@ -1,17 +1,39 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkTs from '../lib/cdk-ts-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as CdkTs from '../lib/cdk-ts-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-ts-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkTs.CdkTsStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('Init S3 Bucket created with correct permissions', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new CdkTs.CdkTsStack(app, 'MyTestStack');
+    // THEN
+    const template = Template.fromStack(stack);
+    template.resourceCountIs('AWS::S3::Bucket', 4);
+    template.resourceCountIs('AWS::S3::BucketPolicy', 4);
+    template.hasResourceProperties('AWS::S3::Bucket', {
+        BucketEncryption: {
+            "ServerSideEncryptionConfiguration": [
+                { "ServerSideEncryptionByDefault": { "SSEAlgorithm": "AES256" } }
+            ],
+        }
+    });
+    template.hasResource('AWS::S3::Bucket', {
+        DeletionPolicy: 'Delete',
+        UpdateReplacePolicy: 'Delete',
+    });
 });
+
+test('TranscribeTriggerFunction has correct base settings', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new CdkTs.CdkTsStack(app, 'MyTestStack');
+    // THEN
+    const template = Template.fromStack(stack);
+    template.resourceCountIs('AWS::Lambda::Function', 2);
+    template.hasResourceProperties('AWS::Lambda::Function', {
+        Handler: 'index.lambdaHandler',
+        Runtime: 'nodejs16.x',
+    });
+
+});
+
